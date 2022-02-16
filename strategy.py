0724c9e8cb9300b8f18ddc85f5f76e625579ac84
cezar-r <59450965+cezar-r@users.noreply.github.com>
2022-02-15 18:08:17 -0700

    Update strategy.py

--- strategy.py
+++ strategy.py
@@ -1,5 +1,5 @@
-
-
+#!/usr/bin/env python
+# coding: utf-8
 
 import matplotlib.pyplot as plt
 from matplotlib.ticker import EngFormatter, StrMethodFormatter
@@ -7,7 +7,6 @@ import numpy as np
 
 
 plt.style.use('ggplot')
-
 plt.style.use("dark_background")
 
 
@@ -15,9 +14,7 @@ starting_leverage = [1.5, 2, 3, 4, 5, 6, 10]
 COINPRICE = 100
 BALANCE = 100_000
 
-
 def total_liquidation():
-
 	for leverage in starting_leverage:
 		og_lev = leverage
 		price = COINPRICE
@@ -36,18 +33,14 @@ def total_liquidation():
 			trades += 1
 
 		fig, ax = plt.subplots()
-
-
-
 		ax.set_title(f'{og_lev}x Leverage\n{round(100  - price, 2)}% Drop before Total Liquidation')
 		ax.plot(x, liquidation_levels)
 		ax.yaxis.set_major_formatter('{x:1.2f}%')
 		ax.set_xlabel("Leveraged Position")
 		ax.set_ylabel("Price")
-		# ax.set_xticklabels([(2**(i))*og_lev for i in x])
 		plt.tight_layout()
-		# plt.savefig(f"imgs/{og_lev}_leverage_tot_liq.png")
-		plt.show()
+		plt.savefig(f"imgs/{og_lev}_leverage_tot_liq.png")
+		# plt.show()
 
 
 
@@ -69,7 +62,6 @@ def to_profit():
 			trades += 1
 
 		fig, ax = plt.subplots()
-
 		ax.set_title(f'{og_lev}x Leverage\n% Move to Break Even')
 		ax.plot(x, to_profit)
 		ax.yaxis.set_major_formatter('{x:1.2f}%')
@@ -98,7 +90,6 @@ def profit_at_breakeven():
 				trades += 1
 
 		fig, ax = plt.subplots()
-
 		ax.set_title(f'{og_lev}x Leverage\n% Gains On Price Breakeven')
 		ax.plot(x, gain_pct)
 		ax.yaxis.set_major_formatter('{x:1.2f}%')
@@ -109,10 +100,10 @@ def profit_at_breakeven():
 		# plt.show()
 
 
-
-# to_profit()
-# total_liquidation()
-profit_at_breakeven()
+if __name__ == '__main__':
+	# to_profit()
+	# total_liquidation()
+	# profit_at_breakeven()
 
 
 '''
